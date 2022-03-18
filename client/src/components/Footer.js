bd88ad82ddd9259047bb78c55725e3cf9b5e6218
Jerrick <73060376+CheddarChzKeys@users.noreply.github.com>
2022-03-18 00:23:12 -0700

    New footer component with CSS (#2)
    * Added Dockerfile and Docker-compose file for container deployment.
    Changed mongo-client get request url to support containerized mongo db.
    Installed regenerator-runtime dependency to support asynchronous functions
    Included .pem key to support ssh and scp commands into ec2 instance.
    Revised docker-compose to include volume for database persistence.
    
    * Checking if my new feature branch *CheddarChzKeys is working
    
    * Remove exposed RSA Key from repo.

---
+++ client/src/components/Footer.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <div className="footerGrid">
+        <div className="modernWarfareWrap">
+          <img className="footerLogo" src="./icons/playstation-logotype.png" />
+        </div>
+        <div className="modernWarfareWrap">
+          <img className="footerLogo" src="./icons/xbox.png" />
+        </div>
+        <div className="modernWarfareWrap">
+          <img className="footerLogo" src="./icons/windows.png" />
+        </div>
+        <div className="footerName">
+          <p>Jerrick Ravelo</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
