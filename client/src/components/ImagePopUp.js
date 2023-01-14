38f5811f7fa0a3dd1a35031c49f2cbc4b9a8bcaf
CheddarChzKeys <jerrick.ravelo@gmail.com>
2023-01-13 21:47:39 -0800

    Gallery component CSS improvements Fixed gallery pagination functionality

--- client/src/components/ImagePopUp.js
+++ client/src/components/ImagePopUp.js
@@ -1,47 +1,6 @@
 import React, { useEffect } from "react";
 
 function ImagePopUp(props) {
-  // useEffect(() => {
-  //   var tag = document.createElement("script");
-  //   tag.src = "https://www.youtube.com/iframe_api";
-  //   console.log("tag:", tag);
-  //   var firstScriptTag = document.getElementsByTagName("script")[0];
-  //   console.log("firstScriptTag:", firstScriptTag);
-  //   firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-  //   function onPlayerReady(event) {
-  //     event.target.playVideo();
-  //   }
-
-  //   var done = false;
-  //   function onPlayerStateChange(event) {
-  //     if (event.data == YT.PlayerState.PLAYING && !done) {
-  //       setTimeout(stopVideo, 100000);
-  //       done = true;
-  //     }
-  //   }
-  //   function stopVideo() {
-  //     player.stopVideo();
-  //   }
-  //   console.log("TrYING to LOAD PLAYER");
-  //   var player;
-
-  //   function onYouTubeIframeAPIReady() {
-  //     console.log("youTube Iframe API REady");
-  //     player = new YT.Player("player", {
-  //       height: "390",
-  //       width: "640",
-  //       videoId: "Y19q-7VN2WI",
-  //       playerVars: {
-  //         playsinline: 1,
-  //       },
-  //       events: {
-  //         onReady: onPlayerReady,
-  //         onStateChange: onPlayerStateChange,
-  //       },
-  //     });
-  //   }
-  // });
   const onClose = () => {
     props.toggleImagePopUp(false);
     props.changePopUpImage(null);
