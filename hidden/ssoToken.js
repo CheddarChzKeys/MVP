eced79ac395963bd42e90432661c8d5909539f82
CheddarChzKeys <jerrick.ravelo@gmail.com>
2024-02-25 22:31:28 -0800

    New call-of-duty-api ssoToken and call-of-duty-api version update

--- hidden/ssoToken.js
+++ hidden/ssoToken.js
@@ -1,5 +1,6 @@
 const SSOtoken =
-  "NjgyNzg2MDk4MzAyNDcwODAxODoxNjcyOTkyMDk0MDYzOmZiZWJkZmE4ZmIwNDgwNDNhYzEyZTljYWMxMTMxYzM1";
+  "NjgyNzg2MDk4MzAyNDcwODAxODoxNzEwMTIyMDEyNDk1OmViMWZhN2YyZGY0YTU4ZTY3YmMyNmMxYjBhNDk2OTNj";
+// NjgyNzg2MDk4MzAyNDcwODAxODoxNzEwMTIyMDEyNDk1OmViMWZhN2YyZGY0YTU4ZTY3YmMyNmMxYjBhNDk2OTNj;
 
 module.exports = {
   token: SSOtoken,
