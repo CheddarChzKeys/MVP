96523e8c7adf842e8abe0e18f29ad4f1981f8f82
CheddarChzKeys <jerrick.ravelo@gmail.com>
2024-03-24 04:05:31 -0700

    MVC Refactoring

---
+++ server/controllers/logOutController.js
@@ -0,0 +1,12 @@
+const dbClient = require("../dbAccess.js");
+
+const logOut = async (req, res) => {
+  db = dbClient.db("warzone");
+  const deadRefreshToken = req.body.deadRefreshToken;
+  const refreshTokens = db.collection("refreshTokens");
+  await refreshTokens.deleteOne({
+    refreshToken: deadRefreshToken,
+  });
+};
+
+module.exports = logOut;
--- server/routes/usersRoutes.js
+++ server/routes/usersRoutes.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
-const verifyController = require("../controllers/verifyController");
+const usersController = require("../controllers/usersController");
+const verifyController = require("../controllers/verifyGamertagController");
 const signUpController = require("../controllers/signUpController");
+const logInController = require("../controllers/logInController");
+const logOutController = require("../controllers/logOutController");
+const verifyTokenController = require("../controllers/verifyTokenController");
 
-router.post("/verify", verifyController);
+router.get("/users"), usersController;
+
+router.post("/verifyGamertag", verifyController);
 router.post("/signUp", signUpController);
+router.post("/logIn", logInController);
+router.post("/logout", logOutController);
+router.post("/verifyToken", verifyTokenController);
 
 module.exports = router;
